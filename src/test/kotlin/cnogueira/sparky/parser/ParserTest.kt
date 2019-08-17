bdd58b8e76fde56b565418a402f8a9593b9daa97
Cris Nogueira <c.nogueira@ocado.com>
2019-08-17 18:53:59 +0200

    Add failing test  to Parser

--- src/test/kotlin/cnogueira/sparky/parser/ParserTest.kt
+++ src/test/kotlin/cnogueira/sparky/parser/ParserTest.kt
@@ -1,10 +1,12 @@
 package cnogueira.sparky.parser
 
 import cnogueira.sparky.exceptions.TokenizeException
+import cnogueira.sparky.grammar.LiteralExpression
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
@@ -23,5 +25,12 @@ internal class ParserTest {
         every { tokenizer.tokenize(any()) } throws TokenizeException("with some message")
 
         assertThrows<TokenizeException> { parser.parse("it really doesn't matter") }
+    }
+
+    @Test
+    internal fun `parse single integer expression`() {
+        every { tokenizer.tokenize("1337") } returns listOf("1337")
+
+        assertEquals(LiteralExpression(1337), parser.parse("1337"))
     }
 }
