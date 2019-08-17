caca61b0a67c5ff5c8e1d30c3a5ab145f8c2ef99
Cris Nogueira <c.nogueira@ocado.com>
2019-08-17 19:05:32 +0200

    Add impl to pass test  to Parser

--- src/main/kotlin/cnogueira/sparky/grammar/LiteralExpression.kt
+++ src/main/kotlin/cnogueira/sparky/grammar/LiteralExpression.kt
@@ -1,7 +1,22 @@
 package cnogueira.sparky.grammar
 
-class LiteralExpression(value: Int) : Expression {
+class LiteralExpression(private val value: Int) : Expression {
     override fun value(): Int {
         TODO("unimplemented")
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LiteralExpression
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value
     }
 }
--- src/main/kotlin/cnogueira/sparky/parser/Parser.kt
+++ src/main/kotlin/cnogueira/sparky/parser/Parser.kt
@@ -1,11 +1,17 @@
 package cnogueira.sparky.parser
 
+import cnogueira.sparky.exceptions.ParseException
 import cnogueira.sparky.grammar.Expression
+import cnogueira.sparky.grammar.LiteralExpression
 
 class Parser(private val tokenizer: Tokenizer) {
     fun parse(input: String): Expression {
         val tokens = tokenizer.tokenize(input)
 
-        TODO("build expression from token list")
+        if (tokens.isNotEmpty()) {
+            return LiteralExpression(Integer.parseInt(tokens.first()))
+        }
+
+        throw ParseException("Expecting an expression")
     }
 }
