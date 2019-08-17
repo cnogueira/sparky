d3e01d2904a33f63663730a48b5a749104a26d6b
Cris Nogueira <c.nogueira@ocado.com>
2019-08-17 19:18:12 +0200

    Add failing test  to Tokenizer

---
+++ src/test/kotlin/cnogueira/sparky/parser/TokenizerTest.kt
@@ -0,0 +1,27 @@
+package cnogueira.sparky.parser
+
+import cnogueira.sparky.exceptions.TokenizeException
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class TokenizerTest {
+
+    private val invalidInput = listOf(
+        "a12b", "1home", "1234.5789b", "15.", "01", "10_0", "10_00", "10_0000", ".2.3", "1.2.3"
+    )
+
+    lateinit var tokenizer: Tokenizer
+
+    @BeforeEach
+    internal fun setUp() {
+        tokenizer = Tokenizer()
+    }
+
+    @Test
+    internal fun `tokenize throws TokenizeException when passing invalid input`() {
+        invalidInput.forEach {
+            assertThrows<TokenizeException> { tokenizer.tokenize(it) }
+        }
+    }
+}
