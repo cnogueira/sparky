4a86853113a85ae19a613240da03f8467a9b5466
Cris Nogueira <c.nogueira@ocado.com>
2019-08-18 17:34:40 +0200

    Add failing test

--- src/test/kotlin/cnogueira/sparky/parser/ParserTest.kt
+++ src/test/kotlin/cnogueira/sparky/parser/ParserTest.kt
@@ -1,5 +1,10 @@
 package cnogueira.sparky.parser
 
+import assertk.assertThat
+import assertk.assertions.hasMessage
+import assertk.assertions.isFailure
+import assertk.assertions.isInstanceOf
+import cnogueira.sparky.exceptions.ParseException
 import cnogueira.sparky.grammar.LiteralExpression
 import cnogueira.sparky.lexer.IntToken
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -20,5 +25,18 @@ internal class ParserTest {
         val tokenList = listOf(IntToken(1, 0, 4, 1337))
 
         assertEquals(LiteralExpression(1337), parser.parse(tokenList))
+    }
+
+    @Test
+    internal fun `throws when encounters two consecutive literals`() {
+        val tokenList = listOf(
+            IntToken(1, 0, 2, 12),
+            IntToken(1, 3, 1, 7)
+        )
+
+        assertThat { parser.parse(tokenList) }
+            .isFailure()
+            .isInstanceOf(ParseException::class)
+            .hasMessage("[1:3..4]: Unexpected token: '7'")
     }
 }
